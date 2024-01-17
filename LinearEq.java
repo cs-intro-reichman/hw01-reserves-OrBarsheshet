a2107d5dd9ec6eb2599ed5f29a6aa7f53d01df31
OrBarsheshet <or.barsheshet@post.runi.ac.il>
2024-01-17 14:21:09 +0200

    first try

--- LinearEq.java
+++ LinearEq.java
@@ -1,9 +1,13 @@
-/*
- * Solves linear equations of the form a⋅x + b = c.
- * The program gets a, b, and c as command-line arguments,
- * computes x, and prints the result.
- * Treats the three arguments as well as the computed value as double values
- */
+package HomeWork1;
 public class LinearEq {
-	// Put your code here
+    public static void main(String[] args) {
+        double a= Double.parseDouble(args[0]);
+        double b= Double.parseDouble(args[1]);
+        double c= Double.parseDouble(args[2]);
+
+        System.out.println(a + " * " + "x " + b + " = " + c);
+        double x = ((c-b)/a); 
+        System.out.println("x" + " = " + x);
+
+    }
 }
--- Triangle.java
+++ Triangle.java
@@ -1,10 +1,13 @@
-/*
- * Three sides can form a triangle if the sum of the lengths of any two sides is greater than the length of the remaining side. 
- * This is known as the Triangle Inequality Theorem. 
- * Write a program that tests if three given integers form a triangle.
- */ 
+package HomeWork1;
 public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+    public static void main(String[] args) {
+        int a= Integer.parseInt(args[0]);
+        int b= Integer.parseInt(args[1]);
+        int c= Integer.parseInt(args[2]);
+        if (a+b>c && a+c>b && b+c> a){
+            System.out.println(a + ", " + b + ", " + c + ": " + " true");
+        }
+        else System.out.println(a + ", " + b + ", " + c + ": " +"false");
+    }
+    
 }
