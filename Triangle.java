c648c6c1ce15e612c1410ff1a1f90b52d09ab386
OrBarsheshet <or.barsheshet@post.runi.ac.il>
2024-01-21 13:36:12 +0200

    Traingle without if

--- Triangle.java
+++ Triangle.java
@@ -3,10 +3,8 @@ public class Triangle {
         int a= Integer.parseInt(args[0]);
         int b= Integer.parseInt(args[1]);
         int c= Integer.parseInt(args[2]);
-        if (a+b>c && a+c>b && b+c> a){
-            System.out.println(a + ", " + b + ", " + c + ": " + "true");
-        }
-        else System.out.println(a + ", " + b + ", " + c + ": " +"false");
+        boolean sumLength = (a+b>c && a+c>b && b+c> a);
+        System.out.println(a + ", " + b + ", " + c + ": " + sumLength);
     }
     
 }
